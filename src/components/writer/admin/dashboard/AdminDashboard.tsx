2ff45065d2a829277aa68f1c968550e870f9dbf8
minhtb99 <minhtb.dev@gmail.com>
2022-01-12 00:12:26 +0700

    fix add feature

--- src/components/writer/admin/dashboard/AdminDashboard.tsx
+++ src/components/writer/admin/dashboard/AdminDashboard.tsx
@@ -1,4 +1,4 @@
-import React, {  useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { getArticlesByAuthorId } from "../../../../apis/service";
@@ -7,11 +7,10 @@ import ArticleForm from "../../handle-news/ArticleForm";
 import AdminHeader from "../admin-header/AdminHeader";
 import ArticleList from "../list-article/ArticleList";
 import AdminProfile from "../profile/AdminProfile";
-import styles from "./AdminDashboard.module.css";
-
 
 export default function AdminDashboard() {
   const [listArticle, setListArticle] = useState<ArticleModal[]>([]);
+  const [load, setLoad] = useState(true);
 
   const getArticleFromApis = async () => {
     const idAuthor = 1;
@@ -24,9 +23,11 @@ export default function AdminDashboard() {
     return () => {
       getArticleFromApis()
     }
-  }, [])
+  }, [load])
 
- 
+  const _handleLoad = () => {
+    setLoad(load => (!load))
+  }
   return (
     <Container>
       <AdminHeader />
@@ -35,7 +36,7 @@ export default function AdminDashboard() {
           <Route path="/" element={<Navigate to={"/admin/manage-article"} />} />
           <Route path="/manage-article" element={<ArticleList listArticle={listArticle} />} />
           <Route path="/profile" element={<AdminProfile />} />
-          <Route path="/create-new" element={<ArticleForm type={'create'}/>} />
+          <Route path="/create-new" element={<ArticleForm type={'create'} reLoad={_handleLoad} />} />
         </Routes>
       </Container>
     </Container>
