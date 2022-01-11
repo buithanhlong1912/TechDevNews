3a54d29ce423d056e3c51941e50b19b69f35ba63
buithanhlong1912 <buithanhlong1912@gmail.com>
2022-01-11 14:36:30 +0700

    11/1 Long commit

---
+++ src/pages/LoginAdmin.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import LoginFormComponent from "../components/writer/admin/Login/LoginFormComponent";
+
+const AdminLogin = () => {
+  return (
+    <>
+      <LoginFormComponent />
+    </>
+  );
+};
+
+export default AdminLogin;
