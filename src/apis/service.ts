337a8a6d5f77cf5e91c7bde7cafc660ec65864a2
Minh Tran <97397674+minhtb99@users.noreply.github.com>
2022-01-13 18:59:23 +0700

    delete feature, formmat date

--- src/apis/service.ts
+++ src/apis/service.ts
@@ -4,21 +4,21 @@ import { ArticleModal, ArticleModalFormAddDTO, IForm } from "../interface";
 axios.defaults.baseURL = 'http://localhost:3000';
 
 export async function getArticles() {
-  try {
-    const response = await axios.get("/articles");
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        const response = await axios.get("/articles");
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 export async function getArticlesByAuthorId(id: number) {
-  try {
-    const response = await axios.get("/articles?authorId=" + id);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        const response = await axios.get("/articles?authorId=" + id);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 export async function getTop4Article() {
@@ -40,36 +40,36 @@ export async function getArticleByCategoryId(id: number) {
 }
 
 export async function createAricle(article: ArticleModalFormAddDTO) {
-  try {
-    const data = {
-      ...article,
-      dateCreate: new Date(),
-      like: 0,
-      disLike: 0,
-      view: 0,
-    };
-    const response = await axios.post("/articles", data);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        const data = {
+            ...article,
+            dateCreate: new Date(),
+            like: 0,
+            disLike: 0,
+            view: 0,
+        };
+        const response = await axios.post("/articles", data);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 export async function login(form: IForm) {
-  const { userName, password } = form;
-  try {
-    const response = await axios.get(`/users?userName=${userName}`);
-    if (response.data.length !== 0) {
-      if (response.data[0].password === password) {
-        return response.data[0];
-      }
-      return false;
-    } else {
-      return false;
+    const { userName, password } = form;
+    try {
+        const response = await axios.get(`/users?userName=${userName}`);
+        if (response.data.length !== 0) {
+            if (response.data[0].password === password) {
+                return response.data[0];
+            }
+            return false;
+        } else {
+            return false;
+        }
+    } catch (error) {
+        console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
 }
 
 export async function getTop4ByView() {
@@ -105,7 +105,12 @@ export async function getArticlesId(id: string | undefined) {
             return false;
         }
         const response = await axios.get('/articles?id=' + id);
-        return response.data;
+        const getUserById = await axios.get('/users?id=' + response.data[0].authorId);
+        const articleDetaile = {
+            ...response.data[0],
+            user: getUserById.data[0]
+        }
+        return articleDetaile;
     } catch (error) {
         console.error(error);
     }
@@ -114,6 +119,15 @@ export async function getArticlesId(id: string | undefined) {
 export async function editArticlesById(article: ArticleModal) {
     try {
         const response = await axios.put('/articles/' + article.id, article);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function deleteArticlesById(id: number) {
+    try {
+        const response = await axios.delete('/articles/' + id);
         return response.data;
     } catch (error) {
         console.error(error);
