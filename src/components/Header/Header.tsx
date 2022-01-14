2fb9ed45e1e38ae3de9f8793c8f57aca9dc008e5
buithanhlong1912 <buithanhlong1912@gmail.com>
2022-01-14 17:02:47 +0700

    test login Google

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,16 +9,29 @@ import {
   Container,
   Form,
   FormControl,
+  Modal,
   Nav,
   Navbar,
   NavDropdown,
 } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import Login from "./login/Login";
+import { GoogleLogin } from "react-google-login";
 
 function Header() {
   const [menuTitle, setTitle] = useState([]);
   const [loggedIn, setLoggedIn] = useState<boolean>(false);
   const [show, setShow] = useState<boolean>(false);
+  const clientId =
+    "421005288141-79gs72nt5s3divhvnm8fritsmjl2gnol.apps.googleusercontent.com";
+
+  const onLoginSuccess = (res: any) => {
+    console.log("Login Success", res.profileObj);
+  };
+
+  const onFailureSuccess = (res: any) => {
+    console.log("Login Failure", res.profileObj);
+  };
 
   const Navigate = useNavigate();
   useEffect(() => {
@@ -30,20 +43,35 @@ function Header() {
   const handleCategory = (id: number) => {
     Navigate(`/home/category/${id}`);
   };
-  const handleHome = ()=>{
-    Navigate(`/home`)
-}
+
+  const handleHome = () => {
+    Navigate(`/home`);
+  };
+
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="sticky-top">
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+      className="sticky-top"
+    >
       <Container>
         <Navbar.Brand href="">
-          <img style={{cursor:"pointer"}} onClick={handleHome} src={logo} className="logoApp" width="100" height="100" />
+          <img
+            style={{ cursor: "pointer" }}
+            onClick={handleHome}
+            src={logo}
+            className="logoApp"
+            width="100"
+            height="100"
+          />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             {menuTitle?.map((item: any, index: number) => (
-              <Nav.Link onClick={() => handleCategory(item.id)}>
+              <Nav.Link key={index} onClick={() => handleCategory(item.id)}>
                 {item.title}
               </Nav.Link>
             ))}
@@ -62,14 +90,31 @@ function Header() {
             </Form>
             &nbsp;&nbsp;&nbsp;
             {!loggedIn ? (
-              <Nav.Link onClick={() => setShow(true)}>
-                <i className="far fa-user-circle"></i>
-              </Nav.Link>
+              <GoogleLogin
+                clientId={clientId}
+                // render={() => (
+                //   <Nav.Link>
+                //     <i className="far fa-user-circle"></i>
+                //   </Nav.Link>
+                // )}
+                buttonText="Login"
+                onSuccess={onLoginSuccess}
+                onFailure={onFailureSuccess}
+                cookiePolicy={"single_host_origin"}
+              />
             ) : (
               <NavDropdown title="Hi, User" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Logout</NavDropdown.Item>
               </NavDropdown>
             )}
+            {/* <Modal show={show} onHide={() => setShow(false)}>
+              <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Login />
+              </Modal.Body>
+            </Modal> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
