c46412f07eee5b3424a1c9ca4aa9dfcfed677c62
buithanhlong1912 <buithanhlong1912@gmail.com>
2022-01-14 18:23:21 +0700

    commit login by Google

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,35 +2,49 @@ import React, { useState, useEffect } from "react";
 import "./Header.css";
 import { getCategoies } from "../../apis/service";
 import img from "../../logo/techdevnews_logo.png";
-import logo from "../../logo/techdevnews_logo.svg";
-
 import {
   Button,
   Container,
   Form,
   FormControl,
-  Modal,
   Nav,
   Navbar,
   NavDropdown,
 } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import Login from "./login/Login";
-import { GoogleLogin } from "react-google-login";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
 
 function Header() {
   const [menuTitle, setTitle] = useState([]);
   const [loggedIn, setLoggedIn] = useState<boolean>(false);
-  const [show, setShow] = useState<boolean>(false);
+  const [user, setUser] = useState({
+    email: "",
+    imageUrl: "",
+    name: "",
+  });
   const clientId =
     "421005288141-79gs72nt5s3divhvnm8fritsmjl2gnol.apps.googleusercontent.com";
 
   const onLoginSuccess = (res: any) => {
-    console.log("Login Success", res.profileObj);
+    localStorage.setItem("user", JSON.stringify(res.profileObj));
+    setUser({
+      email: res.profileObj.email,
+      imageUrl: res.profileObj.imageUrl,
+      name: res.profileObj.name,
+    });
+    setLoggedIn(true);
   };
 
   const onFailureSuccess = (res: any) => {
     console.log("Login Failure", res.profileObj);
+  };
+
+  const onLogout = (res: any) => {
+    setUser({
+      email: "",
+      imageUrl: "",
+      name: "",
+    });
   };
 
   const Navigate = useNavigate();
@@ -57,14 +71,12 @@ function Header() {
       className="sticky-top"
     >
       <Container>
-        <Navbar.Brand href="">
+        <Navbar.Brand onClick={() => handleHome()}>
           <img
             style={{ cursor: "pointer" }}
             onClick={handleHome}
-            src={logo}
+            src={img}
             className="logoApp"
-            width="100"
-            height="100"
           />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -92,29 +104,26 @@ function Header() {
             {!loggedIn ? (
               <GoogleLogin
                 clientId={clientId}
-                // render={() => (
-                //   <Nav.Link>
-                //     <i className="far fa-user-circle"></i>
-                //   </Nav.Link>
-                // )}
+                render={(renderProps) => (
+                  <Nav.Link onClick={renderProps.onClick}>
+                    <i className="far fa-user-circle"></i>
+                  </Nav.Link>
+                )}
                 buttonText="Login"
                 onSuccess={onLoginSuccess}
                 onFailure={onFailureSuccess}
                 cookiePolicy={"single_host_origin"}
               />
             ) : (
-              <NavDropdown title="Hi, User" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Logout</NavDropdown.Item>
+              <NavDropdown title={`${user.name}`} id="collasible-nav-dropdown">
+                {/* <NavDropdown.Item href="#action/3.1">Logout</NavDropdown.Item> */}
+                <GoogleLogout
+                  clientId={clientId}
+                  buttonText="Logout"
+                  onLogoutSuccess={onLogout}
+                ></GoogleLogout>
               </NavDropdown>
             )}
-            {/* <Modal show={show} onHide={() => setShow(false)}>
-              <Modal.Header closeButton>
-                <Modal.Title>Modal heading</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <Login />
-              </Modal.Body>
-            </Modal> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
