94f9a8c8d78edae28b3c1cacc31307e20bd4c1c3
TueTran <{tuemeoa13@gmail.com}>
2022-01-14 12:11:49 +0700

    edit header and articledetails

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,36 +1,46 @@
 import React, { useState, useEffect } from "react";
-import  "./Header.css";
+import "./Header.css";
 import { getCategoies } from "../../apis/service";
-import img from "../../logo/techdevnews_logo.png"
-import { Button, Container, Form, FormControl, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import img from "../../logo/techdevnews_logo.png";
+import {
+  Button,
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
-  const [statusSearch, setStatus] = useState(false);
   const [menuTitle, setTitle] = useState([]);
 
-  function handleClickSearch() {
-    setStatus(!statusSearch);
-  }
+  const Navigate = useNavigate();
   useEffect(() => {
     getCategoies().then((data) => {
       setTitle(data);
     });
   }, []);
 
+  const handleCategory = (id: number) => {
+    Navigate(`/category/${id}`);
+  };
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">
-          <img src={img} className="logoApp"/>
+        <Navbar.Brand>
+          <img src={img} className="logoApp" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Programming</Nav.Link>
-            <Nav.Link href="#pricing">UI/UX</Nav.Link>
-            <Nav.Link href="#pricing">Mobile</Nav.Link>
-            <Nav.Link href="#pricing">Internet</Nav.Link>
-            <Nav.Link href="#pricing">Block Chain</Nav.Link>
+            {menuTitle?.map((item: any, index: number) => (
+              <Nav.Link onClick={() => handleCategory(item.id)}>
+                {item.title}
+              </Nav.Link>
+            ))}
           </Nav>
           <Nav>
             <Form className="d-flex mr-3">
@@ -40,10 +50,12 @@ function Header() {
                 className="me-2"
                 aria-label="Search"
               />
-              <Button variant="btn btn-outline-light"><i className="fas fa-search"></i></Button>
+              <Button variant="btn btn-outline-light">
+                <i className="fas fa-search"></i>
+              </Button>
             </Form>
             &nbsp;&nbsp;&nbsp;
-            {/* <Nav.Link href="#deets"><i className="far fa-user-circle"></i></Nav.Link> Nếu chưa đăng nhập thì icon này*/} 
+            {/* <Nav.Link href="#deets"><i className="far fa-user-circle"></i></Nav.Link> Nếu chưa đăng nhập thì icon này*/}
             <NavDropdown title="Hi, User" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Logout</NavDropdown.Item>
             </NavDropdown>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ArticleDetails from '../components/ArticleDetails/ArticleDetails'
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
-import HomePageComponent from '../components/reader/HomePageComponent';
+import HomePageComponent from '../components/reader/homePageComponent';
 export default function Home() {
   return (
     <div>
